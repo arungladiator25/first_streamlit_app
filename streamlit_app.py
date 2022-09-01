6a37fe0c3609ba2939f0504939dca261812da8e2
arungladiator25 <112605280+arungladiator25@users.noreply.github.com>
2022-09-01 15:16:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-streamlit.text('arunnnnnnnnnnn :) ')
+streamlit.text('arunnnnnnnnnnn :) 🍌')
 
